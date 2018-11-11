c8e365ae8ab9a52e87dcf718501183a61cbeb4c5
Avarokin <avarokins>
2018-11-10 18:59:02 -0600

    hi

--- app/src/main/java/com/test1/avarokin/paltable/SignUp.java
+++ app/src/main/java/com/test1/avarokin/paltable/SignUp.java
@@ -4,14 +4,32 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SignUp extends AppCompatActivity {
+
+    String[] majors = {"Computer Science", "CNIT", "Data Science", "Management","Mathematics"};
+    String[] standings = {"Freshman", "Sophomore", "Junior", "Senior", "Graduate"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Spinner spin = findViewById(R.id.spinner);
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,majors);
+        spin.setAdapter(aa);
+
+
+        Spinner spin2 = findViewById(R.id.spinner2);
+        ArrayAdapter aa2 = new ArrayAdapter(this,android.R.layout.simple_spinner_item,standings);
+        spin.setAdapter(aa2);
+
     }
+
 
     public void crossButton(View view){
 
